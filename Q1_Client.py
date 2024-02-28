a96156f02a2996d2abaff90c94178e082bf4749f
abdirahman Mohamed <toahdini.1999@gmail.com>
2024-02-28 01:02:20 +0000

    Initial Commit

--- Q1_Client.py
+++ Q1_Client.py
@@ -3,6 +3,9 @@ import socket
 import pickle
 import os
 
+HOST = "localhost"  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
 def send_file(filepath):
     try:
         with open(filepath, "rb") as f:
